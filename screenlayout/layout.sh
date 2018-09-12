600adc82f176542b040906e5ca3fab727bcce283
Alexander Chythlook <achythlook@gmail.com>
2018-09-11 16:48:50 -0800

    Add some stuff for Ubuntu setup at work

---
+++ screenlayout/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1080 --pos 0x728 --rotate normal --output DP-2 --mode 2560x1080 --pos 2560x0 --rotate left
