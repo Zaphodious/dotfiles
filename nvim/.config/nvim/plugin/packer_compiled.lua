a72ba76e16c503071d3080960c9ff864561fc5d9
Zaphodious <achythlook@blakwurm.com>
2023-03-24 20:06:46 -0800

    Remove .DS_Store -.-

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -151,7 +151,7 @@ _G.packer_plugins = {
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/zaphodious/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/zaphodious/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
@@ -177,7 +177,7 @@ _G.packer_plugins = {
   ["trouble.nvim"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0" },
     loaded = true,
-    path = "/home/zaphodious/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    path = "/Users/zaphodious/.local/share/nvim/site/pack/packer/start/trouble.nvim",
     url = "https://github.com/folke/trouble.nvim"
   },
   undotree = {
