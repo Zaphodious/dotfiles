b517b7f8761ee9362a655d437a541e350bb3ad0e
Zaphodious <zaph@zaphodious.com>
2023-03-28 21:55:22 -0800

    Add 'Oh My Bash' and adjust setup script slightly

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Get flags
+while getopts b: flag 
+do 
+    case "${flag}" in 
+    b) popshop_is_borked=true;;
+esac
+done
+
 # Set up nix
 if ! command -v nix-env &> /dev/null
 then
+    echo "Nix not detected. Installing." 
     if grep -qi microsoft /proc/version
     then
         sh <(curl -L https://nixos.org/nix/install) --no-daemon
@@ -10,61 +19,104 @@ then
         sh <(curl -L https://nixos.org/nix/install) --daemon
     fi
     . ~/.nix-profile/etc/profile.d/nix.sh
+    echo "Reload the shell and then run setup.sh once again"
+    exit 3 # We can go no further until the shell has been reloaded
 fi
 
 # Install stuff we need
 if ! command -v stow &> /dev/null
 then
-nix-env --install stow
+    echo "Stow not detected. Installing." 
+    nix-env --install stow
 fi
 
 if ! command -v curl &> /dev/null
 then
-nix-env --install curl
+    echo "curl not detected. Installing." 
+    nix-env --install curl
 fi
 
 if ! command -v javac &> /dev/null
 then
-nix-env --install openjdk
+    echo "OpenJDK not detected. Installing." 
+    nix-env --install openjdk
 fi
 
 if ! command -v npm &> /dev/null
 then
+    echo "Node not detected. Installing." 
     nix-env --install nodejs
 fi
 
 if ! command -v gcc &> /dev/null && [[ $OSTYPE != 'darwin' ]];
 then
+    echo "GCC not detected. Installing." 
     nix-env --install gcc
 fi
 
 if ! command -v python3 &> /dev/null
 then
-nix-env --install python3
+    echo "Python3 not detected. Installing." 
+    nix-env --install python3
+fi
+
+if ! command -v git &> /dev/null
+then
+    echo "Git not detected... somehow. Huh. Installing." 
+    nix-env --install git
 fi
 
 if ! command -v cargo &> /dev/null
 then
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-elseDejaVu Sans Mono Book
+    echo "Rust not detected. Installing." 
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo "Reload the shell and then run setup.sh once again"
+    echo 3
+else
+    #DejaVu Sans Mono Book
+    echo "Rust detected. Running rustup update." 
     rustup update
 fi
 
 if ! command -v bob &> /dev/null
 then
-cargo install bob-nvim
-bob install 0.8.3
-bob use 0.8.3
+    echo "Bob not detected. Installing, then installing neovim 0.8.3" 
+    cargo install bob-nvim
+    bob install 0.8.3
+    bob use 0.8.3
 fi
 
 # Install Packer
 if [ -d "~/.local/share/nvim/site/pack/packer/start" ] 
 then
+    echo "Packer not detected. Installing." 
     git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
 
 # Make sure that WSL has access to my dev folder
 if grep -qi microsoft /proc/version && [ -d "~/dev" ] 
 then
+    echo "WSL detected. Symlinking Alex's dev folder."
     ln -s /mnt/c/Users/achyt/dev/ ~/
+fi
+
+# If we've got apt, we want to update it.
+if command -v apt &> /dev/null
+then
+
+    if [ popshop_is_borked == true ]
+    then
+        echo "If Pop! Shop is borked, we need to clean some stuff up"
+        sudo rm -rf /var/lib/apt/lists/
+        sudo apt clean
+        sudo apt autoclean
+        sudo apt update
+        sudo dpkg --configure -a
+        sudo apt --fix-broken-install
+        sudo apt upgrade -y
+    else
+        echo "Running basic apt updates"
+        sudo apt update -y
+        sudo apt upgrade -y
+    fi
 fi
